0bccf168faaf7fbab2fb1df5435caed64fb8a802
devanshmeena422 <devanshmeena422@gmail.com>
2026-02-06 22:43:37 +0530

    Add files via upload

---
+++ variable-03/01basic.py
@@ -0,0 +1,4 @@
+# In python variables are used to store data that can be used and manipulated throughout a program A variable is created the moment you assingn a value to it using the assingment operator(=)
+age = 34 # integer
+name = "devansh" #string
+cgpa = 4.55 # float
