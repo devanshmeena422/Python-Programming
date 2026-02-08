81ad636a1d79085c5c8d8454995686c0283cd3e2
Devansh Meena <devanshmeena422@gmail.com>
2026-02-08 12:13:28 +0530

    Add files via upload

---
+++ typecasting-04/01_typecasting.py
@@ -0,0 +1,14 @@
+a = 34
+b = "64"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# convert b to an integer
+
+c=int(b)
+print(c)
+print(type(c))
---
+++ typecasting-04/practice_typecasting.py
@@ -0,0 +1,16 @@
+a = 80 
+b = 45
+ 
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+c = str(a)
+print(c)
+print(type(c))
+
+d = float(b)
+print(d)
+print(type(d))
