33565702e25e2a2d039c015dedade4b86993e8f2
Devansh Meena <devanshmeena422@gmail.com>
2026-02-08 12:15:23 +0530

    Add files via upload

---
+++ comments_print_ESC-06/01_COMMENTS.py
@@ -0,0 +1,12 @@
+# 01_comments.py
+print("how are you")
+
+# arjun please finish this code
+'''
+Explore how to write comments, use escapes
+sequences, and master the print() statement in Python.
+Learn to document your code with comments, format output using 
+escape characters like \n and \t, and customize print
+behavior for clearer and more structured output.
+SO THATS ALL ABOUT IT
+'''
---
+++ comments_print_ESC-06/02escape.py
@@ -0,0 +1,4 @@
+print("how are you \n i am good\n newline")
+
+print("hello \" World")
+print("Hello \' World")
