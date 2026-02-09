5e92bd63944bd1df98d62cf23e2fb261f0dd2404
Devansh Meena <devanshmeena422@gmail.com>
2026-02-09 20:35:07 +0530

    Add files via upload

---
+++ 09_LOOPS/01_for_lops.py
@@ -0,0 +1,11 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+
+for i in  range(1,11): #it generate the formula of (n-1) so it means (11-1)=10
+    # print(5*(i))
+
+    print("5 *",i,"==", 5*(i))
---
+++ 09_LOOPS/03_infinite_loops.py
@@ -0,0 +1,6 @@
+# i = 1
+# while True:
+#     print(i)
+#     i= i+1
+
+# # dont try to run this type of while lops because it can crash the system by hanging problem or something else
