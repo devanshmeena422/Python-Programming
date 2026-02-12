198dc457a6336a4c7bdfd886428344be2ce1741a
Devansh Meena <devanshmeena422@gmail.com>
2026-02-13 00:20:23 +0530

    new folder

---
+++ 15_DICTIONARIES/01_dictionaries.py
@@ -0,0 +1,4 @@
+marks = {"harry": 45 , "jack" : 74 , "lilly" : 75}
+print(marks,type(marks))
+marks["harry"] = 96
+print(marks)
---
+++ 15_DICTIONARIES/02_dict_methods.py
@@ -0,0 +1,11 @@
+marks = {"harry": 45 , "jack" : 74 , "lilly" : 75}
+print(marks,type(marks))
+marks["harry"] = 96
+print(marks)
+print(marks.keys())
+print(marks.values())
+print(marks.pop("lilly"))
+marks.clear
+print(marks)
+
+
