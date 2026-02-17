038429d7b4651f62b1755b8ec97a76c4a3d1b920
Devansh Meena <devanshmeena422@gmail.com>
2026-02-17 19:00:44 +0530

    new file

---
+++ 17_ADVANCE-PYTHON-CONCEPTS/12_walrus_operator2.py
@@ -0,0 +1,18 @@
+# I HAVE WORK ON THIS CODE 
+
+# while data != "quit":
+#     print(f"You entered: {data}")
+#     data = input("Enter a value (or 'quit' to exit) : ")
+
+# # WITH WALRUS OPERATOR
+
+# while (data :=  input("Enter a value (or 'quit' to exit) : "))
+#     print(f"You entered: {data}")
+
+
+# ANOTHER EXAMPLE
+
+while(data:=input("Enter the value: ")):
+    print(data)
+    if data == "q":
+        break
